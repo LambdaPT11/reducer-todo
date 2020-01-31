cf0dc2142afe437b252fe385092f948e42a0d0f8
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-01-31 13:13:52 -0500

    starting reducer and adding components

---
+++ src/reducers/reducer.js
@@ -0,0 +1,18 @@
+
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Make Sweet Tea',
+        completed: false,
+        id: Date.now()
+    }
+]
+
+export const reducer = (state, action) => {
+    return state;
+}
